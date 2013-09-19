d2e86e890e7283b9fef436dbecf25eb1353693bc
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:28:32 +0200

    various (next release will be 0.6)

--- _define.php
+++ _define.php
@@ -1,20 +1,21 @@
 <?php
-# -- BEGIN LICENSE BLOCK ---------------------------------------
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of dmLastComments, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) Franck Paul and contributors
+# carnet.franck.paul@gmail.com
 #
-# Copyright (c) 2012 Franck Paul
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
-# -- END LICENSE BLOCK -----------------------------------------
+# -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
 	/* Name */			"Last Comments Dashboard Module",
 	/* Description*/		"Display last comments on dashboard",
 	/* Author */			"Franck Paul",
-	/* Version */			'0.5'
+	/* Version */			'0.6'
 );
 ?>
