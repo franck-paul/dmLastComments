29eab5c0b48159ae0b1c1f561d7896178bbda5bc
franck <carnet.franck.paul@gmail.com>
2013-10-04 08:36:05 +0200

    new _define.php

--- _define.php
+++ _define.php
@@ -16,5 +16,9 @@ $this->registerModule(
 	/* Name */			"Last Comments Dashboard Module",
 	/* Description*/		"Display last comments on dashboard",
 	/* Author */			"Franck Paul",
-	/* Version */			'0.6'
+	/* Version */			'0.6',
+	array(
+		/* Permissions */	'permissions' =>	'admin',
+		/* Type */			'type' =>			'plugin'
+	)
 );
