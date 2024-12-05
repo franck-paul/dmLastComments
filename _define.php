ca8790116e9ad1c8b0bcb4e58b2f59ad09cf90f4
franck <carnet.franck.paul@gmail.com>
2024-12-05 16:40:58 +0100

    New release 6.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Last Comments Dashboard Module',
     'Display last comments on dashboard',
     'Franck Paul',
-    '5.5.1',
+    '6.0',
     [
         'requires'    => [['core', '2.33']],
         'permissions' => 'My',
