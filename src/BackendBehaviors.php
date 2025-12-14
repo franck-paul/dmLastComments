638060902730e94c1939450b21fe67aead362887
franck <carnet.franck.paul@gmail.com>
2025-12-14 08:55:03 +0100

    A11y: add en empty alt attribute to icon image in module title

--- src/BackendBehaviors.php
+++ src/BackendBehaviors.php
@@ -215,6 +215,7 @@ class BackendBehaviors
                     (new Text(
                         'h3',
                         (new Img(urldecode((string) App::backend()->page()->getPF(My::id() . '/icon.svg'))))
+                            ->alt('')
                             ->class('icon-small')
                         ->render() .
                         ' ' . __('Last comments')
