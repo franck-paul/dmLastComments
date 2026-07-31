744d1e7f8fb75d6c34202e79d40e073571efe376
franck <contact@open-time.net>
2026-07-31 18:12:16 +0200

    Defer loaded script execution (wait for dmHelper)

--- js/service.js
+++ js/service.js
@@ -2,13 +2,6 @@
 'use strict';
 
 dotclear.ready(() => {
-  /**
-   * Check connection
-   *
-   * @return     {?boolean}  true/false depending on connection, null if cannot be tested
-   */
-  dotclear.dmOnline = () => ('onLine' in navigator ? navigator.onLine : null);
-
   dotclear.dmLastComments = dotclear.getData('dm_lastcomments');
 
   const viewComment = (line, _action = 'toggle', event = null) => {
